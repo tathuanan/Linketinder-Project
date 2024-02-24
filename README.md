820e291a2492e89b7659b242b161f79636bae085
Murillo Santos <murillo-santos@outlook.com.br>
2024-02-24 13:52:07 -0300

    Melhorado visualização em terminal e atualização do README.md

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# Linketinder-Project
-Projeto Linketinder AceleraZG
+# Linketinder
+
+### Introdução
+
+Projeto Linketinder que está sendo desenvolvido no programa de capacitação AceleraZG
+
+O projeto consiste em combinar o sistema de "double opt-in" (Match do Tinder) com o sistema de rede social do LinkedIn.
+
+Assim caso seja estabelecido um "Match" entre a empresa recrutadora e o candidato, eles poderiam iniciar uma intereção.
+
+No atual estado do programa existe as seguintes funcionalidades:
+
+    Listar Candidatos.
+    Listar Empresas.
+    Encerrar aplicação.
+
+### Como executar
+
+Seu computador deverá ter o ambiente Groovy.
+
+A aplicação inicia com a execução da classe "Main.groovy"
+
+Por [Murillo Santos](https://br.linkedin.com/in/murillo-santos-6a179a1b8)
--- src/com/acelerazg/aplicacao/Menu.groovy
+++ src/com/acelerazg/aplicacao/Menu.groovy
@@ -10,13 +10,15 @@ class Menu {
         Scanner ler = new Scanner(System.in)
 
         int opcao
-
+        String bl = "\n"
         String menuPrincipal =
-                "Bem-vindo ao Menu Principal do LinkeTinder\n\n" +
-                        "Por favor escolha uma opção:\n" +
-                        "1. Listar Candidatos.\n" +
-                        "2. Listar Empresas.\n" +
-                        "3. Encerrar aplicação."
+               "\nPor favor escolha uma opção:\n" +
+                       "1. Listar Candidatos.\n" +
+                       "2. Listar Empresas.\n" +
+                       "3. Encerrar aplicação."
+
+        println "Bem-vindo ao Menu Principal do LinkeTinder"
+
         while (true) {
             println menuPrincipal
             opcao = Integer.parseInt(ler.nextLine())
@@ -25,16 +27,18 @@ class Menu {
 
                 switch (opcao) {
                     case 1:
+                        println bl
                         ControladorCandidatos.listarCandidatos()
                         break
                     case 2:
+                        println bl
                         ControladorEmpresas.listarEmpresas()
                         break
                     case 3:
-                        println "Encerrando a aplicação"
+                        println "\nEncerrando a aplicação"
                         break
                     default:
-                        println("Por favor escolha uma opção válida")
+                        println("\nOpção inválida!!")
                         break
                 }
 
