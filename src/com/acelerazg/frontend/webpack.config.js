6f76f20b88b5ab7eb857559c75fe56d0f7167f57
Murillo Santos <murillo-santos@outlook.com.br>
2024-03-09 16:59:52 -0300

    Ambiente preparado com webpack para utilização de "modulos

---
+++ src/com/acelerazg/frontend/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const CopyPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: './recursos/Main.ts',
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist')
+        },
+        port: 9000,
+        hot: true,
+    },
+    output: {
+        filename: 'app.min.js',
+        path: path.join(__dirname, 'dist')
+    },
+    plugins: [
+      new CopyPlugin({
+          patterns: [{from: 'publico'}]
+      }),
+    ],
+    resolve: {
+        extensions: [ '.ts', '.js']
+    },
+    module: {
+        rules: [{
+            test: /\.ts$/,
+            use: 'ts-loader',
+            exclude: /node_modules/
+        }]
+    }
+
+}
