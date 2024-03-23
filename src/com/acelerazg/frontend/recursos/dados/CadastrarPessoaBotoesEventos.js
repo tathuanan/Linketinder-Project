a30e509bf65f0f888da555d57862de99209ba972
Murillo Santos <murillo-santos@outlook.com.br>
2024-03-23 10:17:26 -0300

    Correções no cadastro de empresa e candidato

--- src/com/acelerazg/frontend/recursos/dados/CadastrarPessoaBotoesEventos.js
+++ src/com/acelerazg/frontend/recursos/dados/CadastrarPessoaBotoesEventos.js
@@ -26,12 +26,8 @@ class CadastrarPessoaBotoesEventos {
     static cadastrarCandidato() {
         const formCandidato = document.getElementById('formCandidato');
         if (formCandidato != formCandidato.getAttribute('hidden')) {
-            formCandidato.addEventListener('submit', function () {
+            formCandidato.addEventListener('submit', function (event) {
                 let competencias = document.querySelectorAll('#candidatoCompetencias input[type="checkbox"]:checked');
-                if (competencias.length === 0) {
-                    formCandidato.preventDefault();
-                    alert('Selecione pelo menos uma competência.');
-                }
                 let candidatoNome = document.getElementById('candidatoNome');
                 let cpf = document.getElementById('cpf');
                 let idade = document.getElementById('idade');
@@ -40,48 +36,50 @@ class CadastrarPessoaBotoesEventos {
                 let cep = document.getElementById('candidatoCep');
                 let descricao = document.getElementById('candidatoDescricao');
                 let competenciasSelecionadas;
-                function obterCompetenciasSelecionadas() {
-                    competenciasSelecionadas = [];
-                    const checkboxes = document.querySelectorAll('#candidatoCompetencias input[type="checkbox"]:checked');
-                    checkboxes.forEach(function (checkbox) {
-                        competenciasSelecionadas.push(checkbox.getAttribute('value'));
-                    });
-                    return competenciasSelecionadas;
+                if (competencias.length === 0) {
+                    event.preventDefault();
+                    alert('Selecione pelo menos uma competência.');
                 }
-                competenciasSelecionadas = obterCompetenciasSelecionadas();
-                competenciasSelecionadas.join(', ');
-                competenciasSelecionadas.toString();
-                localStorage.setItem('candidatoNome', candidatoNome.value);
-                localStorage.setItem('cpf', cpf.value);
-                localStorage.setItem('idade', idade.value);
-                localStorage.setItem('email', email.value);
-                localStorage.setItem('estado', estado.value);
-                localStorage.setItem('cep', cep.value);
-                localStorage.setItem('descricao', descricao.value);
-                localStorage.setItem('competencias', competenciasSelecionadas.toString());
-                candidatoNome = localStorage.getItem('candidatoNome');
-                email = localStorage.getItem('email');
-                estado = localStorage.getItem('estado');
-                descricao = localStorage.getItem('descricao');
-                competencias = localStorage.getItem('competencias');
-                cep = localStorage.getItem('cep');
-                cpf = localStorage.getItem('cpf');
-                idade = localStorage.getItem('idade');
-                ListaPessoa_1.ListaPessoa.candidatos.push(new Candidato_1.Candidato(candidatoNome, email, estado, descricao, competencias, cep, cpf, idade));
-                alert("Candidato cadastrado com sucesso!!");
+                else {
+                    function obterCompetenciasSelecionadas() {
+                        competenciasSelecionadas = [];
+                        const checkboxes = document.querySelectorAll('#candidatoCompetencias input[type="checkbox"]:checked');
+                        checkboxes.forEach(function (checkbox) {
+                            competenciasSelecionadas.push(checkbox.getAttribute('value'));
+                        });
+                        return competenciasSelecionadas;
+                    }
+                    competenciasSelecionadas = obterCompetenciasSelecionadas();
+                    competenciasSelecionadas.join(', ');
+                    competenciasSelecionadas.toString();
+                    localStorage.setItem('candidatoNome', candidatoNome.value);
+                    localStorage.setItem('cpf', cpf.value);
+                    localStorage.setItem('idade', idade.value);
+                    localStorage.setItem('email', email.value);
+                    localStorage.setItem('estado', estado.value);
+                    localStorage.setItem('cep', cep.value);
+                    localStorage.setItem('descricao', descricao.value);
+                    localStorage.setItem('competencias', competenciasSelecionadas.toString());
+                    candidatoNome = localStorage.getItem('candidatoNome');
+                    email = localStorage.getItem('email');
+                    estado = localStorage.getItem('estado');
+                    descricao = localStorage.getItem('descricao');
+                    competencias = localStorage.getItem('competencias');
+                    cep = localStorage.getItem('cep');
+                    cpf = localStorage.getItem('cpf');
+                    idade = localStorage.getItem('idade');
+                    ListaPessoa_1.ListaPessoa.candidatos.push(new Candidato_1.Candidato(candidatoNome, email, estado, descricao, competencias, cep, cpf, idade));
+                    alert("Candidato cadastrado com sucesso!!");
+                }
             });
         }
     }
     static cadastrarEmpresa() {
         const formEmpresa = document.getElementById('formEmpresa');
         if (formEmpresa != formEmpresa.getAttribute('hidden')) {
-            formEmpresa.addEventListener('submit', function () {
+            formEmpresa.addEventListener('submit', function (event) {
                 let competencias = document.querySelectorAll('#empresaCompetencias input[type="checkbox"]:checked');
-                if (competencias.length === 0) {
-                    formEmpresa.preventDefault();
-                    alert('Selecione pelo menos uma competência.');
-                }
-                let empresaNome = document.getElementById('empresaNomeNome');
+                let empresaNome = document.getElementById('empresaNome');
                 let cnpj = document.getElementById('cnpj');
                 let pais = document.getElementById('pais');
                 let email = document.getElementById('empresaEmail');
@@ -89,35 +87,41 @@ class CadastrarPessoaBotoesEventos {
                 let cep = document.getElementById('empresaCep');
                 let descricao = document.getElementById('empresaDescricao');
                 let competenciasSelecionadas;
-                function obterCompetenciasSelecionadas() {
-                    competenciasSelecionadas = [];
-                    const checkboxes = document.querySelectorAll('#empresaCompetencias input[type="checkbox"]:checked');
-                    checkboxes.forEach(function (checkbox) {
-                        competenciasSelecionadas.push(checkbox.getAttribute('value'));
-                    });
-                    return competenciasSelecionadas;
+                if (competencias.length === 0) {
+                    event.preventDefault();
+                    alert('Selecione pelo menos uma competência.');
                 }
-                competenciasSelecionadas = obterCompetenciasSelecionadas();
-                competenciasSelecionadas.join(', ');
-                competenciasSelecionadas.toString();
-                localStorage.setItem('empresaNome', empresaNome.value);
-                localStorage.setItem('cnpj', cnpj.value);
-                localStorage.setItem('pais', pais.value);
-                localStorage.setItem('empresaEmail', email.value);
-                localStorage.setItem('empresaEstado', estado.value);
-                localStorage.setItem('empresaCep', cep.value);
-                localStorage.setItem('empresaDescricao', descricao.value);
-                localStorage.setItem('empresaCompetencias', competenciasSelecionadas.toString());
-                empresaNome = localStorage.getItem('empresaNome');
-                email = localStorage.getItem('empresaEmail');
-                estado = localStorage.getItem('empresaEstado');
-                descricao = localStorage.getItem('empresaDescricao');
-                competencias = localStorage.getItem('empresaCompetencias');
-                cep = localStorage.getItem('empresaCep');
-                cnpj = localStorage.getItem('cnpj');
-                pais = localStorage.getItem('pais');
-                ListaPessoa_1.ListaPessoa.empresas.push(new Empresa_1.Empresa(empresaNome, email, estado, descricao, competencias, cep, pais, cnpj));
-                alert("Empresa cadastrada com sucesso!!");
+                else {
+                    function obterCompetenciasSelecionadas() {
+                        competenciasSelecionadas = [];
+                        const checkboxes = document.querySelectorAll('#empresaCompetencias input[type="checkbox"]:checked');
+                        checkboxes.forEach(function (checkbox) {
+                            competenciasSelecionadas.push(checkbox.getAttribute('value'));
+                        });
+                        return competenciasSelecionadas;
+                    }
+                    competenciasSelecionadas = obterCompetenciasSelecionadas();
+                    competenciasSelecionadas.join(', ');
+                    competenciasSelecionadas.toString();
+                    localStorage.setItem('empresaNome', empresaNome.value);
+                    localStorage.setItem('cnpj', cnpj.value);
+                    localStorage.setItem('pais', pais.value);
+                    localStorage.setItem('empresaEmail', email.value);
+                    localStorage.setItem('empresaEstado', estado.value);
+                    localStorage.setItem('empresaCep', cep.value);
+                    localStorage.setItem('empresaDescricao', descricao.value);
+                    localStorage.setItem('empresaCompetencias', competenciasSelecionadas.toString());
+                    empresaNome = localStorage.getItem('empresaNome');
+                    email = localStorage.getItem('empresaEmail');
+                    estado = localStorage.getItem('empresaEstado');
+                    descricao = localStorage.getItem('empresaDescricao');
+                    competencias = localStorage.getItem('empresaCompetencias');
+                    cep = localStorage.getItem('empresaCep');
+                    cnpj = localStorage.getItem('cnpj');
+                    pais = localStorage.getItem('pais');
+                    ListaPessoa_1.ListaPessoa.empresas.push(new Empresa_1.Empresa(empresaNome, email, estado, descricao, competencias, cep, pais, cnpj));
+                    alert("Empresa cadastrada com sucesso!!");
+                }
             });
         }
     }
--- src/com/acelerazg/frontend/recursos/dados/CadastrarPessoaBotoesEventos.ts
+++ src/com/acelerazg/frontend/recursos/dados/CadastrarPessoaBotoesEventos.ts
@@ -1,6 +1,5 @@
 import {ListaPessoa} from "./ListaPessoa";
 import {Candidato} from "../pessoas/Candidato";
-import SubmitEvent = JQuery.SubmitEvent;
 import {Empresa} from "../pessoas/Empresa";
 
 export class CadastrarPessoaBotoesEventos {
@@ -35,67 +34,69 @@ export class CadastrarPessoaBotoesEventos {
 
         if (formCandidato != formCandidato.getAttribute('hidden')) {
 
-            formCandidato.addEventListener('submit', function () {
-                let competencias: any = document.querySelectorAll('#candidatoCompetencias input[type="checkbox"]:checked');
+            formCandidato.addEventListener('submit', function (event: { preventDefault: () => void; }) {
 
-                if (competencias.length === 0) {
-                    formCandidato.preventDefault();
-                    alert('Selecione pelo menos uma competência.');
+                    let competencias: any = document.querySelectorAll('#candidatoCompetencias input[type="checkbox"]:checked');
+                    let candidatoNome: any = document.getElementById('candidatoNome');
+                    let cpf: any = document.getElementById('cpf');
+                    let idade: any = document.getElementById('idade');
+                    let email: any = document.getElementById('candidatoEmail');
+                    let estado: any = document.getElementById('candidatoEstado');
+                    let cep: any = document.getElementById('candidatoCep');
+                    let descricao: any = document.getElementById('candidatoDescricao');
+                    let competenciasSelecionadas: any[];
+
+                    if (competencias.length === 0) {
+                        event.preventDefault();
+                        alert('Selecione pelo menos uma competência.');
+
+                    } else {
+                        function obterCompetenciasSelecionadas() {
+                            competenciasSelecionadas = [];
+                            const checkboxes = document.querySelectorAll('#candidatoCompetencias input[type="checkbox"]:checked');
+
+                            checkboxes.forEach(function (checkbox) {
+                                competenciasSelecionadas.push(checkbox.getAttribute('value'));
+                            });
+                            return competenciasSelecionadas;
+                        }
+
+                        competenciasSelecionadas = obterCompetenciasSelecionadas();
+                        competenciasSelecionadas.join(', ');
+                        competenciasSelecionadas.toString();
+
+                        localStorage.setItem('candidatoNome', candidatoNome.value);
+                        localStorage.setItem('cpf', cpf.value);
+                        localStorage.setItem('idade', idade.value);
+                        localStorage.setItem('email', email.value);
+                        localStorage.setItem('estado', estado.value);
+                        localStorage.setItem('cep', cep.value);
+                        localStorage.setItem('descricao', descricao.value);
+                        localStorage.setItem('competencias', competenciasSelecionadas.toString());
+
+                        candidatoNome = localStorage.getItem('candidatoNome');
+                        email = localStorage.getItem('email');
+                        estado = localStorage.getItem('estado');
+                        descricao = localStorage.getItem('descricao');
+                        competencias = localStorage.getItem('competencias');
+                        cep = localStorage.getItem('cep');
+                        cpf = localStorage.getItem('cpf');
+                        idade = localStorage.getItem('idade');
+
+                        ListaPessoa.candidatos.push(new Candidato(
+                            candidatoNome,
+                            email,
+                            estado,
+                            descricao,
+                            competencias,
+                            cep,
+                            cpf,
+                            idade))
+
+                        alert("Candidato cadastrado com sucesso!!");
+                    }
                 }
-
-                let candidatoNome: any = document.getElementById('candidatoNome');
-                let cpf: any = document.getElementById('cpf');
-                let idade: any = document.getElementById('idade');
-                let email: any = document.getElementById('candidatoEmail');
-                let estado: any = document.getElementById('candidatoEstado');
-                let cep: any = document.getElementById('candidatoCep');
-                let descricao: any = document.getElementById('candidatoDescricao');
-                let competenciasSelecionadas: any[];
-
-                function obterCompetenciasSelecionadas() {
-                    competenciasSelecionadas = [];
-                    const checkboxes = document.querySelectorAll('#candidatoCompetencias input[type="checkbox"]:checked');
-
-                    checkboxes.forEach(function (checkbox) {
-                        competenciasSelecionadas.push(checkbox.getAttribute('value'));
-                    });
-                    return competenciasSelecionadas;
-                }
-
-                competenciasSelecionadas = obterCompetenciasSelecionadas();
-                competenciasSelecionadas.join(', ');
-                competenciasSelecionadas.toString();
-
-                localStorage.setItem('candidatoNome', candidatoNome.value);
-                localStorage.setItem('cpf', cpf.value);
-                localStorage.setItem('idade', idade.value);
-                localStorage.setItem('email', email.value);
-                localStorage.setItem('estado', estado.value);
-                localStorage.setItem('cep', cep.value);
-                localStorage.setItem('descricao', descricao.value);
-                localStorage.setItem('competencias', competenciasSelecionadas.toString());
-
-                candidatoNome = localStorage.getItem('candidatoNome');
-                email = localStorage.getItem('email');
-                estado = localStorage.getItem('estado');
-                descricao = localStorage.getItem('descricao');
-                competencias = localStorage.getItem('competencias');
-                cep = localStorage.getItem('cep');
-                cpf = localStorage.getItem('cpf');
-                idade = localStorage.getItem('idade');
-
-                ListaPessoa.candidatos.push(new Candidato(
-                    candidatoNome,
-                    email,
-                    estado,
-                    descricao,
-                    competencias,
-                    cep,
-                    cpf,
-                    idade)
-                )
-                alert("Candidato cadastrado com sucesso!!");
-            });
+            )
         }
     }
 
@@ -105,67 +106,69 @@ export class CadastrarPessoaBotoesEventos {
 
         if (formEmpresa != formEmpresa.getAttribute('hidden')) {
 
-            formEmpresa.addEventListener('submit', function () {
-                let competencias: any = document.querySelectorAll('#empresaCompetencias input[type="checkbox"]:checked');
+            formEmpresa.addEventListener('submit', function (event: { preventDefault: () => void; }) {
 
-                if (competencias.length === 0) {
-                    formEmpresa.preventDefault();
-                    alert('Selecione pelo menos uma competência.');
+                    let competencias: any = document.querySelectorAll('#empresaCompetencias input[type="checkbox"]:checked');
+                    let empresaNome: any = document.getElementById('empresaNome');
+                    let cnpj: any = document.getElementById('cnpj');
+                    let pais: any = document.getElementById('pais');
+                    let email: any = document.getElementById('empresaEmail');
+                    let estado: any = document.getElementById('empresaEstado');
+                    let cep: any = document.getElementById('empresaCep');
+                    let descricao: any = document.getElementById('empresaDescricao');
+                    let competenciasSelecionadas: any[];
+
+                    if (competencias.length === 0) {
+                        event.preventDefault();
+                        alert('Selecione pelo menos uma competência.');
+
+                    } else {
+                        function obterCompetenciasSelecionadas() {
+                            competenciasSelecionadas = [];
+                            const checkboxes = document.querySelectorAll('#empresaCompetencias input[type="checkbox"]:checked');
+
+                            checkboxes.forEach(function (checkbox) {
+                                competenciasSelecionadas.push(checkbox.getAttribute('value'));
+                            })
+                            return competenciasSelecionadas;
+                        }
+
+                        competenciasSelecionadas = obterCompetenciasSelecionadas();
+                        competenciasSelecionadas.join(', ');
+                        competenciasSelecionadas.toString();
+
+                        localStorage.setItem('empresaNome', empresaNome.value);
+                        localStorage.setItem('cnpj', cnpj.value);
+                        localStorage.setItem('pais', pais.value);
+                        localStorage.setItem('empresaEmail', email.value);
+                        localStorage.setItem('empresaEstado', estado.value);
+                        localStorage.setItem('empresaCep', cep.value);
+                        localStorage.setItem('empresaDescricao', descricao.value);
+                        localStorage.setItem('empresaCompetencias', competenciasSelecionadas.toString());
+
+                        empresaNome = localStorage.getItem('empresaNome');
+                        email = localStorage.getItem('empresaEmail');
+                        estado = localStorage.getItem('empresaEstado');
+                        descricao = localStorage.getItem('empresaDescricao');
+                        competencias = localStorage.getItem('empresaCompetencias');
+                        cep = localStorage.getItem('empresaCep');
+                        cnpj = localStorage.getItem('cnpj');
+                        pais = localStorage.getItem('pais');
+
+                        ListaPessoa.empresas.push(new Empresa(
+                            empresaNome,
+                            email,
+                            estado,
+                            descricao,
+                            competencias,
+                            cep,
+                            pais,
+                            cnpj))
+
+                        alert("Empresa cadastrada com sucesso!!");
+                    }
                 }
-
-                let empresaNome: any = document.getElementById('empresaNomeNome');
-                let cnpj: any = document.getElementById('cnpj');
-                let pais: any = document.getElementById('pais');
-                let email: any = document.getElementById('empresaEmail');
-                let estado: any = document.getElementById('empresaEstado');
-                let cep: any = document.getElementById('empresaCep');
-                let descricao: any = document.getElementById('empresaDescricao');
-                let competenciasSelecionadas: any[];
-
-                function obterCompetenciasSelecionadas() {
-                    competenciasSelecionadas = [];
-                    const checkboxes = document.querySelectorAll('#empresaCompetencias input[type="checkbox"]:checked');
-
-                    checkboxes.forEach(function (checkbox) {
-                        competenciasSelecionadas.push(checkbox.getAttribute('value'));
-                    });
-                    return competenciasSelecionadas;
-                }
-
-                competenciasSelecionadas = obterCompetenciasSelecionadas();
-                competenciasSelecionadas.join(', ');
-                competenciasSelecionadas.toString();
-
-                localStorage.setItem('empresaNome', empresaNome.value);
-                localStorage.setItem('cnpj', cnpj.value);
-                localStorage.setItem('pais', pais.value);
-                localStorage.setItem('empresaEmail', email.value);
-                localStorage.setItem('empresaEstado', estado.value);
-                localStorage.setItem('empresaCep', cep.value);
-                localStorage.setItem('empresaDescricao', descricao.value);
-                localStorage.setItem('empresaCompetencias', competenciasSelecionadas.toString());
-
-                empresaNome = localStorage.getItem('empresaNome');
-                email = localStorage.getItem('empresaEmail');
-                estado = localStorage.getItem('empresaEstado');
-                descricao = localStorage.getItem('empresaDescricao');
-                competencias = localStorage.getItem('empresaCompetencias');
-                cep = localStorage.getItem('empresaCep');
-                cnpj = localStorage.getItem('cnpj');
-                pais = localStorage.getItem('pais');
-
-                ListaPessoa.empresas.push(new Empresa(
-                    empresaNome,
-                    email,
-                    estado,
-                    descricao,
-                    competencias,
-                    cep,
-                    pais,
-                    cnpj)
-                );
-                alert("Empresa cadastrada com sucesso!!");
-            });
+            )
         }
     }
 }
