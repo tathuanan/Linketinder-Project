979a5370af09f2476c70ab152b95ce07e9d59444
Murillo Santos <murillo-santos@outlook.com.br>
2024-03-25 03:58:12 -0300

    Incluído CandidatoService

---
+++ src/com/acelerazg/backend/service/CandidatoService.groovy
@@ -0,0 +1,62 @@
+package com.acelerazg.backend.service
+
+import com.acelerazg.backend.DAO.CandidatoDAO
+import com.acelerazg.backend.model.Candidato
+
+class CandidatoService {
+
+    static listarCandidatos(){
+
+        CandidatoDAO candidatoDAO = new CandidatoDAO()
+        candidatoDAO.listar().each { candidato ->
+            println candidato.toString()
+        }
+
+    }
+
+    static cadastrarCandidato(Candidato candidato) {
+
+        boolean retornoDB
+
+        CandidatoDAO candidatoDAO = new CandidatoDAO()
+        retornoDB = candidatoDAO.inserir(candidato)
+
+        if (retornoDB){
+            println("\nCandidato cadastrada com sucesso!!!")
+        } else {
+            println("\nOcorreu um erro no cadastro")
+        }
+
+    }
+
+    static alterarCandidato(Candidato candidato) {
+
+        boolean retornoDB
+
+        CandidatoDAO candidatoDAO = new CandidatoDAO()
+        retornoDB = candidatoDAO.alterar(candidato)
+
+        if (retornoDB){
+            println("\nCandidato alterado com sucesso!!!")
+        } else {
+            println("\nOcorreu um erro na alteração")
+        }
+
+    }
+
+    static deletarCandidato(Integer id) {
+
+        boolean retornoDB
+
+        CandidatoDAO candidatoDAO = new CandidatoDAO()
+        retornoDB = candidatoDAO.remover(id)
+
+        if (retornoDB){
+            println("\nCandidato deletado com sucesso!!!")
+        } else {
+            println("\nOcorreu um erro ao tentar deletar")
+        }
+
+    }
+    
+}
