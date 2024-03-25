f379a873a70614c406e48b46e2f7ce7b0a144686
Murillo Santos <murillo-santos@outlook.com.br>
2024-03-24 23:13:01 -0300

    Incluído model Competencia e Vaga

---
+++ src/com/acelerazg/backend/model/Competencia.groovy
@@ -0,0 +1,14 @@
+package com.acelerazg.backend.model
+
+class Competencia {
+
+    int id
+    String competencia
+
+    String toString() {
+        "Competencia(" +
+                " id=${id},"+
+                " competencia=${competencia}"
+    }
+
+}
---
+++ src/com/acelerazg/backend/model/Vaga.groovy
@@ -0,0 +1,20 @@
+package com.acelerazg.backend.model
+
+class Vaga {
+
+    int id
+    String nome
+    String descricao
+    String cidade
+    String empresa
+
+    String toString() {
+        "Vaga(" +
+            " id=${id}," +
+            " nome=${nome}," +
+            " descricao=${descricao}," +
+            " cidade=${cidade}," +
+            " empresa=${empresa}"
+    }
+
+}
