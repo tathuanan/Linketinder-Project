60c0109675160922e8e25699023ab0bf594899ba
Murillo Santos <murillo-santos@outlook.com.br>
2024-03-26 00:51:45 -0300

    Finalizado lógica VagaDAO incluído método de inserir competencia da vaga

--- src/com/acelerazg/backend/DAO/VagaDAO.groovy
+++ src/com/acelerazg/backend/DAO/VagaDAO.groovy
@@ -38,10 +38,31 @@ class VagaDAO {
         try {
             PreparedStatement stmt = connection.prepareStatement(sql)
             ResultSet resultado = stmt.executeQuery()
+
             while (resultado.next()) {
+
                 Vaga vaga = new Vaga()
                 vaga.setId(resultado.getInt("id"))
                 vaga.setNome(resultado.getString("nome"))
+                vaga.setDescricao(resultado.getString("descricao"))
+                vaga.setCidade(resultado.getString("cidade_id"))
+                vaga.setEmpresa(resultado.getString("empresa_id"))
+
+                List<String> competencias = new ArrayList<>();
+                String sqlCompetencias = "SELECT comp.competencia " +
+                        "FROM competencias_vaga AS comp_v " +
+                        "JOIN competencias AS comp ON comp.id = comp_v.competencia_id " +
+                        "WHERE comp_c.vaga_id = ?";
+                PreparedStatement stmtCompetencias = connection.prepareStatement(sqlCompetencias);
+                stmtCompetencias.setInt(1, vaga.getId());
+                ResultSet resultadoCompetencias = stmtCompetencias.executeQuery();
+
+                while (resultadoCompetencias.next()) {
+                    competencias.add(resultadoCompetencias.getString("competencia"));
+                }
+
+                vaga.setCompetencias(competencias);
+
                 retorno.add(vaga)
             }
         } catch (Exception e) {
@@ -53,10 +74,29 @@ class VagaDAO {
     }
 
     boolean inserir(Vaga vaga) {
-        String sql = "INSERT INTO vagas(nome) VALUES (?)"
+        String sql = "INSERT INTO vagas(nome, descricao, cidade_id, empresa_id) VALUES (?,?,?,?)"
         try {
             PreparedStatement stmt = connection.prepareStatement(sql)
             stmt.setString(1, vaga.getNome())
+            stmt.setString(2, vaga.getDescricao())
+            stmt.setInt(3, Integer.parseInt(vaga.getCidade()))
+            stmt.setInt(4, Integer.parseInt(vaga.getEmpresa()))
+            stmt.execute()
+            return true
+        } catch (Exception e) {
+            e.printStackTrace()
+            return false
+        } finally {
+            connection.close()
+        }
+    }
+
+    boolean inserirCompetenciaVaga(int vaga_id, int competencia_id) {
+        String sql = "INSERT INTO competencias_vaga(vaga_id, competencia_id) VALUES (?,?)"
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql)
+            stmt.setInt(1, vaga_id)
+            stmt.setInt(2, competencia_id)
             stmt.execute()
             return true
         } catch (Exception e) {
@@ -68,11 +108,14 @@ class VagaDAO {
     }
 
     boolean alterar(Vaga vaga) {
-        String sql = "UPDATE vagas SET vaga=? WHERE id=?"
+        String sql = "UPDATE vagas SET nome=?, descricao=?, cidade_id=?, empresa_id=? WHERE id=?"
         try {
             PreparedStatement stmt = connection.prepareStatement(sql)
             stmt.setString(1, vaga.getNome())
-            stmt.setInt(2, vaga.getId())
+            stmt.setString(2, vaga.getDescricao())
+            stmt.setInt(3, Integer.parseInt(vaga.getCidade()))
+            stmt.setInt(4, Integer.parseInt(vaga.getEmpresa()))
+            stmt.setInt(5, vaga.getId())
             stmt.execute()
             return true
         } catch (Exception e) {
