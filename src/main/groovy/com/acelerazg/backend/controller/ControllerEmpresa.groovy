47e5c99fe34b3ec4ed1167b8fd2959cfbe9ba5e4
Murillo Santos <murillo-santos@outlook.com.br>
2024-04-09 23:25:48 -0300

    Refactoring: Desacoplamento da camada viewer da controller Empresa

--- src/main/groovy/com/acelerazg/backend/controller/ControllerEmpresa.groovy
+++ src/main/groovy/com/acelerazg/backend/controller/ControllerEmpresa.groovy
@@ -6,116 +6,30 @@ import com.acelerazg.backend.service.EmpresaService
 
 class ControllerEmpresa {
 
-    private Empresa novaEmpresa = new Empresa()
-    private int opcao
-    private String nomeEmpresa
-    private String cnpj
-    private String email
-    private String cep
-    private String descricao
-    private String senha
-    private String pais
-    private String estado
+    EmpresaService empresaService = new EmpresaService()
+    private boolean retornoDB
 
     void listarEmpresas(){
-        EmpresaService.listarEmpresas()
+
+        this.empresaService.listarEmpresas()
     }
 
-    void cadastrarEmpresa(){
+    boolean cadastrarEmpresa(Empresa empresa){
 
-        println("Digite o nome da empresa:")
-        this.nomeEmpresa = Ferramentas.ler.nextLine()
-
-        println("Digite o cnpj da empresa:")
-        this.cnpj = Ferramentas.ler.nextLine()
-
-        println("Digite o email da empresa:")
-        this.email = Ferramentas.ler.nextLine()
-
-        println("Digite o cep da empresa:")
-        this.cep = Ferramentas.ler.nextLine()
-
-        println("Faça uma descrição da empresa:")
-        this.descricao = Ferramentas.ler.nextLine()
-
-        println("Selecione o pais:\n1. Brasil")
-        this.pais = Integer.parseInt(Ferramentas.ler.nextLine())
-
-        println("Selecione o estado:")
-        println(Ferramentas.estados)
-        this.estado = Integer.parseInt(Ferramentas.ler.nextLine())
-
-        println("Digite uma senha:")
-        this.senha = Ferramentas.ler.nextLine()
-
-        this.novaEmpresa.setNome(this.nomeEmpresa)
-        this.novaEmpresa.setCnpj(this.cnpj)
-        this.novaEmpresa.setEmail(this.email)
-        this.novaEmpresa.setCep(this.cep)
-        this.novaEmpresa.setDescricao(this.descricao)
-        this.novaEmpresa.setPais(this.pais)
-        this.novaEmpresa.setEstado(this.estado)
-        this.novaEmpresa.setSenha(this.senha)
-
-        EmpresaService.cadastrarEmpresa(this.novaEmpresa)
-
+        this.retornoDB = this.empresaService.cadastrarEmpresa(empresa)
+        return this.retornoDB
     }
 
-    void alterarEmpresa(){
+    boolean alterarEmpresa(Empresa empresa){
 
-        EmpresaService.listarEmpresas()
-
-        println "\nEscolha qual empresa você deseja alterar:"
-        this.opcao = Integer.parseInt(Ferramentas.ler.nextLine())
-
-        println("Altere o nome da empresa:")
-        this.nomeEmpresa = Ferramentas.ler.nextLine()
-
-        println("Altere o cnpj da empresa:")
-        this.cnpj = Ferramentas.ler.nextLine()
-
-        println("Altere email da empresa:")
-        this.email = Ferramentas.ler.nextLine()
-
-        println("Altere o cep da empresa:")
-        this.cep = Ferramentas.ler.nextLine()
-
-        println("Altere a descrição da empresa:")
-        this.descricao = Ferramentas.ler.nextLine()
-
-        println("Altere o pais:\n1. Brasil")
-        this.pais = Integer.parseInt(Ferramentas.ler.nextLine())
-
-        println("Altere o estado:")
-        println(Ferramentas.estados)
-        this.estado = Integer.parseInt(Ferramentas.ler.nextLine())
-
-        println("Altere a senha:")
-        this.senha = Ferramentas.ler.nextLine()
-
-        this.novaEmpresa.setId(opcao)
-        this.novaEmpresa.setNome(this.nomeEmpresa)
-        this.novaEmpresa.setCnpj(this.cnpj)
-        this.novaEmpresa.setEmail(this.email)
-        this.novaEmpresa.setCep(this.cep)
-        this.novaEmpresa.setDescricao(this.descricao)
-        this.novaEmpresa.setPais(this.pais)
-        this.novaEmpresa.setEstado(this.estado)
-        this.novaEmpresa.setSenha(this.senha)
-
-        EmpresaService.alterarEmpresa(this.novaEmpresa)
-
+        this.retornoDB = this.empresaService.alterarEmpresa(empresa)
+        return this.retornoDB
     }
 
-    void deletarEmpresa(){
+    boolean deletarEmpresa(Empresa empresa){
 
-        EmpresaService.listarEmpresas()
-
-        println "\nEscolha qual empresa você deseja deletar:"
-        this.opcao = Integer.parseInt(Ferramentas.ler.nextLine())
-
-        EmpresaService.deletarEmpresa(this.opcao)
-
+        this.retornoDB = this.empresaService.deletarEmpresa(empresa)
+        return this.retornoDB
     }
 
 }
